d4a7ae42ca0a0ed9e513c486657a691fd0828436
Christian Murray <mrmurray253@gmail.com>
2022-12-29 11:40:28 -0500

    add api routes for submitting data to create a proof and verifying a proof

--- app.js
+++ app.js
@@ -1,10 +1,37 @@
 const express = require('express')
+//const nfnc = require('./nfnc.js')
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: false }));
+
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.sendFile('index.html', { root: './html' })
 })
+
+app.post('/submitted-data', (req, res) => {
+  var walletAddress = req.body.wallet
+  var contractAddress = req.body.contract
+  var pkey = req.body.privkey
+
+  const { proof, vKey, publicSignals } = await nfnc.generateProof(walletAddress, contractAddress, pkey)
+  // stringify these results and send them back to the user
+  res.send('success!')
+})
+
+app.get('/verify', (req, res) => {
+  res.sendFile('verify.html', {root: './html'} )
+});
+
+app.post('/submit-verify', (req, res) => {
+  var proof = req.body.proof
+  var publicSignal = req.body.publicsignal
+  var vKey = req.body.vkey
+
+  const verificationResult = await nfnc.verify(proof, vKey, publicSignal)
+  res.send(verificationResult)
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
